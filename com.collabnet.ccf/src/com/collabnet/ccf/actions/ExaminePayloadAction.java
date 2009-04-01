7f7be5fc585dab29c574e1f81dec9c88fe8bd2cd
selsemore <selsemore@collab.net>
2009-04-01 20:17:14 +0000

    Examine Hospital payload in default XML editor.  Not yet editable.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/branches/GUIConsole@2302 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- com.collabnet.ccf/src/com/collabnet/ccf/actions/ExaminePayloadAction.java
+++ com.collabnet.ccf/src/com/collabnet/ccf/actions/ExaminePayloadAction.java
@@ -1,14 +1,58 @@
 package com.collabnet.ccf.actions;
 
+import java.util.Iterator;
+
+import org.eclipse.core.resources.IStorage;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Display;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorDescriptor;
+import org.eclipse.ui.IEditorRegistry;
+import org.eclipse.ui.IStorageEditorInput;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.actions.ActionDelegate;
 
-public class ExaminePayloadAction extends ActionDelegate {
+import com.collabnet.ccf.Activator;
+import com.collabnet.ccf.editors.StringInput;
+import com.collabnet.ccf.editors.StringStorage;
+import com.collabnet.ccf.model.Hospital;
 
+public class ExaminePayloadAction extends ActionDelegate {
+	private IStructuredSelection fSelection;
+
+	@SuppressWarnings("unchecked")
 	public void run(IAction action) {
-		MessageDialog.openInformation(Display.getDefault().getActiveShell(), "Examine Payload", "Not yet implemented.");
+		Iterator iter = fSelection.iterator();
+		while (iter.hasNext()) {
+			Object object = iter.next();
+			if (object instanceof Hospital) {
+				IWorkbenchPage page = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				Hospital hospital = (Hospital)object;
+				IStorage storage = new StringStorage(hospital.getGenericArtifact(), hospital.getTimeStamp());
+				IStorageEditorInput input = new StringInput(storage);
+				IEditorRegistry registry = Activator.getDefault().getWorkbench().getEditorRegistry();
+				IEditorDescriptor descriptor = registry.getDefaultEditor("file.xml");
+				String id;
+				if (descriptor == null) {
+					id = "org.eclipse.ui.DefaultTextEditor"; //$NON-NLS-1$
+				} else {
+					id = descriptor.getId();
+				}
+				try {
+					page.openEditor(input, id);
+				} catch (PartInitException e) {
+					Activator.handleError("Examine Hospital Payload", e);
+					break;
+				}
+			}
+		}
+	}	
+	
+	public void selectionChanged(IAction action, ISelection sel) {
+		if (sel instanceof IStructuredSelection) {
+			fSelection= (IStructuredSelection) sel;
+		}
 	}	
 	
 }
---
+++ com.collabnet.ccf/src/com/collabnet/ccf/editors/StringInput.java
@@ -0,0 +1,41 @@
+package com.collabnet.ccf.editors;
+
+import org.eclipse.core.resources.IStorage;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.PlatformObject;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.IPersistableElement;
+import org.eclipse.ui.IStorageEditorInput;
+
+public class StringInput extends PlatformObject implements IStorageEditorInput {
+	private IStorage storage;
+	
+	public StringInput(IStorage storage) {
+		this.storage = storage;
+	}
+
+	public IStorage getStorage() throws CoreException {
+		return storage;
+	}
+
+	public boolean exists() {
+		return true;
+	}
+
+	public ImageDescriptor getImageDescriptor() {
+		return null;
+	}
+
+	public String getName() {
+		return storage.getName();
+	}
+
+	public IPersistableElement getPersistable() {
+		return null;
+	}
+
+	public String getToolTipText() {
+		return getName();
+	}
+
+}
