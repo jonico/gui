91543db30fb0f70d6b103d11240c037ed9544c64
selsemore <selsemore@collab.net>
2009-04-03 15:51:51 +0000

    Menu option to open hospital stack trace in text editor.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/branches/GUIConsole@2317 6771e1e2-0f46-0410-b5d7-d846ff89f743

---
+++ com.collabnet.ccf/src/com/collabnet/ccf/actions/HospitalStackTraceAction.java
@@ -0,0 +1,64 @@
+package com.collabnet.ccf.actions;
+
+import java.util.Iterator;
+
+import org.eclipse.core.resources.IStorage;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IStorageEditorInput;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.actions.ActionDelegate;
+
+import com.collabnet.ccf.Activator;
+import com.collabnet.ccf.editors.StringInput;
+import com.collabnet.ccf.editors.StringStorage;
+import com.collabnet.ccf.model.Patient;
+
+public class HospitalStackTraceAction extends ActionDelegate {
+	private IStructuredSelection fSelection;
+	
+	@SuppressWarnings("unchecked")
+	public void run(IAction action) {
+		Iterator iter = fSelection.iterator();
+		while (iter.hasNext()) {
+			Object object = iter.next();
+			if (object instanceof Patient) {
+				IWorkbenchPage page = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				Patient patient = (Patient)object;
+				IStorage storage = new StringStorage(patient.getStackTrace(), patient.getTimeStamp());
+				IStorageEditorInput input = new StringInput(storage);
+				try {
+					page.openEditor(input, "org.eclipse.ui.DefaultTextEditor");
+				} catch (PartInitException e) {
+					Activator.handleError("Hospital Stack Trace", e);
+					break;
+				}
+			}
+		}
+	}	
+	
+	public void selectionChanged(IAction action, ISelection sel) {
+		if (sel instanceof IStructuredSelection) {
+			fSelection= (IStructuredSelection) sel;
+			if (action != null) action.setEnabled(isEnabledForSelection());
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	private boolean isEnabledForSelection() {
+		if (fSelection == null) return false;
+		Iterator iter = fSelection.iterator();
+		while (iter.hasNext()) {
+			Object object = iter.next();
+			if (object instanceof Patient) {
+				Patient patient = (Patient)object;
+				if (patient.getStackTrace() == null || patient.getStackTrace().length() == 0)
+					return false;
+			}
+		}
+		return true;
+	}	
+
+}
