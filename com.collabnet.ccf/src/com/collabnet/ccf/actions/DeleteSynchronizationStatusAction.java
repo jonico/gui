aeddb07d830d4387fdb022aad71050cb46c64e75
selsemore <selsemore@collab.net>
2009-04-27 20:17:46 +0000

    Delete project mapping option.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/branches/GUIConsole@2450 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- com.collabnet.ccf/src/com/collabnet/ccf/actions/DeleteSynchronizationStatusAction.java
+++ com.collabnet.ccf/src/com/collabnet/ccf/actions/DeleteSynchronizationStatusAction.java
@@ -1,17 +1,65 @@
 package com.collabnet.ccf.actions;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.actions.ActionDelegate;
 
+import com.collabnet.ccf.Activator;
+import com.collabnet.ccf.db.CcfDataProvider;
+import com.collabnet.ccf.db.Filter;
+import com.collabnet.ccf.model.ProjectMappings;
+import com.collabnet.ccf.model.SynchronizationStatus;
+import com.collabnet.ccf.views.CcfExplorerView;
+
 public class DeleteSynchronizationStatusAction extends ActionDelegate {
 	private IStructuredSelection fSelection;
+	private boolean mappingsDeleted;
 	
 	public void run(IAction action) {
-		MessageDialog.openInformation(Display.getDefault().getActiveShell(), "Delete Project Mapping", "Not yet implemented.");
+		if (!MessageDialog.openConfirm(Display.getDefault().getActiveShell(), "Delete Project Mapping", "Delete the selected project mappings?")) return;
+		final List<ProjectMappings> projectMappingsList = new ArrayList<ProjectMappings>();
+		mappingsDeleted = false;
+		final CcfDataProvider dataProvider = new CcfDataProvider();
+		BusyIndicator.showWhile(Display.getDefault(), new Runnable() {
+			@SuppressWarnings("unchecked")
+			public void run() {
+				Iterator iter = fSelection.iterator();
+				while (iter.hasNext()) {
+					Object object = iter.next();
+					if (object instanceof SynchronizationStatus) {
+						SynchronizationStatus status = (SynchronizationStatus)object;
+						Filter sourceSystemFilter = new Filter(CcfDataProvider.SYNCHRONIZATION_STATUS_SOURCE_SYSTEM_ID, status.getSourceSystemId(), true);
+						Filter sourceRepositoryFilter = new Filter(CcfDataProvider.SYNCHRONIZATION_STATUS_SOURCE_REPOSITORY_ID, status.getSourceRepositoryId(), true);
+						Filter targetSystemFilter = new Filter(CcfDataProvider.SYNCHRONIZATION_STATUS_TARGET_SYSTEM_ID, status.getTargetSystemId(), true);
+						Filter targetRepositoryFilter = new Filter(CcfDataProvider.SYNCHRONIZATION_STATUS_TARGET_REPOSITORY_ID, status.getTargetRepositoryId(), true);
+						Filter[] filters = { sourceSystemFilter, sourceRepositoryFilter, targetSystemFilter, targetRepositoryFilter };
+						try {
+							dataProvider.deleteSynchronizationStatuses(filters);
+							mappingsDeleted = true;
+							if (!projectMappingsList.contains(status.getProjectMappings())) {
+								projectMappingsList.add(status.getProjectMappings());
+							}
+						} catch (Exception e) {
+							Activator.handleError(e);
+							break;
+						}
+					}
+				}
+			}			
+		});
+		if (mappingsDeleted && CcfExplorerView.getView() != null) {
+			for (ProjectMappings projectMappings: projectMappingsList) {
+				CcfExplorerView.getView().refresh(projectMappings);
+			}			
+		}
 	}
 
 	public void selectionChanged(IAction action, ISelection sel) {
