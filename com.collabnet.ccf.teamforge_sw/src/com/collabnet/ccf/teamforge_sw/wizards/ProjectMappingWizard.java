d4d11271896cff4319b9b28f905754e856735601
selsemore <selsemore@collab.net>
2010-03-24 18:08:27 +0000

    Implemented Project Mapping wizard Finish button.
    git-svn-id: https://ctf.open.collab.net/svn/repos/ccf/trunk/gui@3280 6771e1e2-0f46-0410-b5d7-d846ff89f743

--- com.collabnet.ccf.teamforge_sw/src/com/collabnet/ccf/teamforge_sw/wizards/ProjectMappingWizard.java
+++ com.collabnet.ccf.teamforge_sw/src/com/collabnet/ccf/teamforge_sw/wizards/ProjectMappingWizard.java
@@ -1,12 +1,19 @@
 package com.collabnet.ccf.teamforge_sw.wizards;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Properties;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.wizard.Wizard;
 
 import com.collabnet.ccf.Activator;
+import com.collabnet.ccf.db.CcfDataProvider;
 import com.collabnet.ccf.model.Landscape;
 import com.collabnet.ccf.model.ProjectMappings;
+import com.collabnet.ccf.model.SynchronizationStatus;
 import com.collabnet.ccf.teamforge.schemageneration.TFSoapClient;
 import com.collabnet.teamforge.api.main.ProjectRow;
 import com.collabnet.teamforge.api.tracker.TrackerRow;
@@ -48,6 +55,100 @@ public class ProjectMappingWizard extends Wizard {
 
 	@Override
 	public boolean performFinish() {
+		IRunnableWithProgress runnable = new IRunnableWithProgress() {
+			public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+				CcfDataProvider dataProvider = new CcfDataProvider();
+				String taskName = "Creating project mappings";
+				monitor.setTaskName(taskName);
+				monitor.beginTask(taskName, 6);
+				
+				monitor.subTask(previewPage.getTrackerTaskMapping());
+				SynchronizationStatus projectMapping = new SynchronizationStatus();
+				projectMapping.setGroup(projectMappings.getLandscape().getGroup());
+				projectMapping.setSourceRepositoryKind("TRACKER");
+				projectMapping.setTargetRepositoryKind("TRACKER");
+				if (projectMappings.getLandscape().getEncoding2() != null && projectMappings.getLandscape().getEncoding2().trim().length() > 0) {
+					projectMapping.setTargetSystemEncoding(projectMappings.getLandscape().getEncoding2());
+				}				
+				if (projectMappings.getLandscape().getType1().equals("TF")) {
+					projectMapping.setSourceSystemId(projectMappings.getLandscape().getId1());	
+					projectMapping.setTargetSystemId(projectMappings.getLandscape().getId2());			
+					projectMapping.setSourceSystemKind(projectMappings.getLandscape().getType1());			
+					projectMapping.setTargetSystemKind(projectMappings.getLandscape().getType2());
+					projectMapping.setSourceSystemTimezone(projectMappings.getLandscape().getTimezone1());
+					projectMapping.setTargetSystemTimezone(projectMappings.getLandscape().getTimezone2());
+				} else {
+					projectMapping.setSourceSystemId(projectMappings.getLandscape().getId2());	
+					projectMapping.setTargetSystemId(projectMappings.getLandscape().getId1());			
+					projectMapping.setSourceSystemKind(projectMappings.getLandscape().getType2());			
+					projectMapping.setTargetSystemKind(projectMappings.getLandscape().getType1());
+					projectMapping.setSourceSystemTimezone(projectMappings.getLandscape().getTimezone2());
+					projectMapping.setTargetSystemTimezone(projectMappings.getLandscape().getTimezone1());					
+				}
+				projectMapping.setSourceRepositoryId(getSelectedTaskTracker().getId());
+				projectMapping.setTargetRepositoryId(getSelectedProduct().getName() + "-Task");
+				projectMapping.setConflictResolutionPriority(previewPage.getTrackerTaskConflictResolutionPriority());
+				createMapping(projectMapping, dataProvider);
+				monitor.worked(1);
+				
+				monitor.subTask(previewPage.getTrackerPbiMapping());
+				projectMapping.setSourceRepositoryId(getSelectedPbiTracker().getId());
+				projectMapping.setTargetRepositoryId(getSelectedProduct().getName() + "-PBI");
+				projectMapping.setConflictResolutionPriority(previewPage.getTrackerPbiConflictResolutionPriority());
+				createMapping(projectMapping, dataProvider);
+				monitor.worked(1);
+				
+				monitor.subTask(previewPage.getPlanningFolderProductMapping());
+				projectMapping.setSourceRepositoryId(getSelectedProject().getId() + "-planningFolders");
+				projectMapping.setTargetRepositoryId(getSelectedProduct().getName() + "-Product");
+				projectMapping.setConflictResolutionPriority(previewPage.getPlanningFolderProductConflictResolutionPriority());
+				createMapping(projectMapping, dataProvider);
+				monitor.worked(1);
+				
+				monitor.subTask(previewPage.getTaskTrackerMapping());
+				if (projectMappings.getLandscape().getType1().equals("SW")) {
+					projectMapping.setSourceSystemId(projectMappings.getLandscape().getId1());	
+					projectMapping.setTargetSystemId(projectMappings.getLandscape().getId2());			
+					projectMapping.setSourceSystemKind(projectMappings.getLandscape().getType1());			
+					projectMapping.setTargetSystemKind(projectMappings.getLandscape().getType2());
+					projectMapping.setSourceSystemTimezone(projectMappings.getLandscape().getTimezone1());
+					projectMapping.setTargetSystemTimezone(projectMappings.getLandscape().getTimezone2());
+				} else {
+					projectMapping.setSourceSystemId(projectMappings.getLandscape().getId2());	
+					projectMapping.setTargetSystemId(projectMappings.getLandscape().getId1());			
+					projectMapping.setSourceSystemKind(projectMappings.getLandscape().getType2());			
+					projectMapping.setTargetSystemKind(projectMappings.getLandscape().getType1());
+					projectMapping.setSourceSystemTimezone(projectMappings.getLandscape().getTimezone2());
+					projectMapping.setTargetSystemTimezone(projectMappings.getLandscape().getTimezone1());					
+				}
+				projectMapping.setSourceRepositoryId(getSelectedProduct().getName() + "-Task");
+				projectMapping.setTargetRepositoryId(getSelectedTaskTracker().getId());
+				projectMapping.setConflictResolutionPriority(previewPage.getTaskTrackerConflictResolutionPriority());
+				createMapping(projectMapping, dataProvider);
+				monitor.worked(1);
+				
+				monitor.subTask(previewPage.getPbiTrackerMapping());
+				projectMapping.setSourceRepositoryId(getSelectedProduct().getName() + "-PBI");
+				projectMapping.setTargetRepositoryId(getSelectedPbiTracker().getId());
+				projectMapping.setConflictResolutionPriority(previewPage.getPbiTrackerConflictResolutionPriority());
+				createMapping(projectMapping, dataProvider);
+				monitor.worked(1);
+				
+				monitor.subTask(previewPage.getProductPlanningFolderMapping());
+				projectMapping.setSourceRepositoryId(getSelectedProduct().getName() + "-Product");
+				projectMapping.setTargetRepositoryId(getSelectedProject().getId() + "-planningFolders");
+				projectMapping.setConflictResolutionPriority(previewPage.getProductPlanningFolderConflictResolutionPriority());
+				createMapping(projectMapping, dataProvider);
+				monitor.worked(1);
+				
+				monitor.done();
+			}
+		};
+		try {
+			getContainer().run(true, false, runnable);
+		} catch (Exception e) {
+			Activator.handleError(e);
+		}
 		return true;
 	}
 	
@@ -88,6 +189,36 @@ public class ProjectMappingWizard extends Wizard {
 			}
 		}
 		return soapClient;
+	}
+
+	private void createMapping(SynchronizationStatus status, CcfDataProvider dataProvider) {
+		status.setSourceSystemKind(status.getSourceSystemKind() + "_paused");
+		try {
+			dataProvider.addSynchronizationStatus(projectMappings, status);
+		} catch (Exception e) {
+			Activator.handleError(e);
+			return;
+		}
+		if (projectMappings.getLandscape().getRole() == Landscape.ROLE_ADMINISTRATOR) {
+			createFieldMappingFile(status);
+		}
+	}
+	
+	private void createFieldMappingFile(SynchronizationStatus status) {
+		status.setLandscape(projectMappings.getLandscape());
+		status.clearXslInfo();
+		File xslFile = status.getXslFile();
+		if (!xslFile.exists()) {
+			try {
+				xslFile.createNewFile();
+				File sampleFile = status.getSampleXslFile();
+				if (sampleFile != null && sampleFile.exists()) {
+					CcfDataProvider.copyFile(sampleFile, xslFile);
+				}
+			} catch (IOException e) {
+				Activator.handleError(e);
+			}
+		}
 	}
 
 }
--- com.collabnet.ccf.teamforge_sw/src/com/collabnet/ccf/teamforge_sw/wizards/ProjectMappingWizardPreviewPage.java
+++ com.collabnet.ccf.teamforge_sw/src/com/collabnet/ccf/teamforge_sw/wizards/ProjectMappingWizardPreviewPage.java
@@ -1,70 +1,95 @@
 package com.collabnet.ccf.teamforge_sw.wizards;
 
-import org.eclipse.jface.viewers.ColumnLayoutData;
-import org.eclipse.jface.viewers.ColumnWeightData;
-import org.eclipse.jface.viewers.IStructuredContentProvider;
-import org.eclipse.jface.viewers.ITableLabelProvider;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.TableLayout;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 import com.collabnet.ccf.Activator;
+import com.collabnet.ccf.model.SynchronizationStatus;
 
 public class ProjectMappingWizardPreviewPage extends WizardPage {
-	private Table table;
-	private TableViewer viewer;
-	
-	private String[] mappings;
-	
 	private String product;
 	private String projectId;
 	private String taskTracker;
 	private String pbiTracker;
+	private String trackerTaskMapping;
+	private String trackerPbiMapping;
+	private String planningFolderProductMapping;
+	private String taskTrackerMapping;
+	private String pbiTrackerMapping;
+	private String productPlanningFolderMapping;
 	
-	private String[] columnHeaders = {"Project Mapping"};
-	private ColumnLayoutData columnLayouts[] = {
-		new ColumnWeightData(450, 450, true)};
+	private Text trackerTaskText;
+	private Text trackerPbiText;
+	private Text planningFolderProductText;
+	private Text taskTrackerText;
+	private Text pbiTrackerText;
+	private Text productPlanningFolderText;
+	
+	private Combo trackerTaskCombo;
+	private Combo trackerPbiCombo;
+	private Combo planningFolderProductCombo;
+	private Combo taskTrackerCombo;
+	private Combo pbiTrackerCombo;
+	private Combo productPlanningFolderCombo;
+	
+	private int selectionIndex;
 
 	public ProjectMappingWizardPreviewPage() {
 		super("previewPage", "Mappings Preview", Activator.getDefault().getImageDescriptor(Activator.IMAGE_NEW_PROJECT_MAPPING_WIZBAN));
-		setPageComplete(true);
+		setPageComplete(false);
 	}
 
 	public void createControl(Composite parent) {
 		Composite outerContainer = new Composite(parent,SWT.NONE);
-		outerContainer.setLayout(new GridLayout());
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+		outerContainer.setLayout(layout);
 		outerContainer.setLayoutData(
 		new GridData(GridData.GRAB_HORIZONTAL | GridData.HORIZONTAL_ALIGN_FILL));
 		
-		table = new Table(outerContainer, SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION);
-		table.setLinesVisible(true);
-		GridData gd = new GridData(GridData.GRAB_HORIZONTAL | GridData.HORIZONTAL_ALIGN_FILL | GridData.GRAB_VERTICAL | GridData.VERTICAL_ALIGN_FILL);
-		gd.widthHint = 500;
-		gd.heightHint = 200;
-		table.setLayoutData(gd);
-		TableLayout tableLayout = new TableLayout();
-		table.setLayout(tableLayout);
-		table.setHeaderVisible(true);
-		viewer = new TableViewer(table);		
-		viewer.setContentProvider(new MappingsContentProvider());
-		viewer.setLabelProvider(new MappingsLabelProvider());
-		for (int i = 0; i < columnHeaders.length; i++) {
-			tableLayout.addColumnData(columnLayouts[i]);
-			TableColumn tc = new TableColumn(table, SWT.NONE,i);
-			tc.setResizable(columnLayouts[i].resizable);
-			tc.setText(columnHeaders[i]);
-		}
-		viewer.setInput(this);	
-
+		Label mappingLabel = new Label(outerContainer, SWT.NONE);
+		mappingLabel.setText("Project Mapping:");
+		
+		Label conflictLabel = new Label(outerContainer, SWT.NONE);
+		conflictLabel.setText("Conflict Resolution:");
+		
+		trackerTaskText = new Text(outerContainer, SWT.READ_ONLY | SWT.BORDER);
+		trackerTaskText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
+		trackerTaskCombo = new Combo(outerContainer, SWT.READ_ONLY);
+		populateConflictResolutionCombo(trackerTaskCombo);
+		
+		trackerPbiText = new Text(outerContainer, SWT.READ_ONLY | SWT.BORDER);
+		trackerPbiText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
+		trackerPbiCombo = new Combo(outerContainer, SWT.READ_ONLY);
+		populateConflictResolutionCombo(trackerPbiCombo);
+		
+		planningFolderProductText = new Text(outerContainer, SWT.READ_ONLY | SWT.BORDER);
+		planningFolderProductText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
+		planningFolderProductCombo = new Combo(outerContainer, SWT.READ_ONLY);
+		populateConflictResolutionCombo(planningFolderProductCombo);
+		
+		taskTrackerText = new Text(outerContainer, SWT.READ_ONLY | SWT.BORDER);
+		taskTrackerText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
+		taskTrackerCombo = new Combo(outerContainer, SWT.READ_ONLY);
+		populateConflictResolutionCombo(taskTrackerCombo);
+		
+		pbiTrackerText = new Text(outerContainer, SWT.READ_ONLY | SWT.BORDER);
+		pbiTrackerText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
+		pbiTrackerCombo = new Combo(outerContainer, SWT.READ_ONLY);
+		populateConflictResolutionCombo(pbiTrackerCombo);
+		
+		productPlanningFolderText = new Text(outerContainer, SWT.READ_ONLY | SWT.BORDER);
+		productPlanningFolderText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL | GridData.GRAB_HORIZONTAL));
+		productPlanningFolderCombo = new Combo(outerContainer, SWT.READ_ONLY);
+		populateConflictResolutionCombo(productPlanningFolderCombo);
+		
 		setMessage("The following mappings will be created.");
 
 		setControl(outerContainer);
@@ -82,50 +107,90 @@ public class ProjectMappingWizardPreviewPage extends WizardPage {
 				projectId = ((ProjectMappingWizard)getWizard()).getSelectedProject().getId();
 				taskTracker = ((ProjectMappingWizard)getWizard()).getSelectedTaskTracker().getId();
 				pbiTracker = ((ProjectMappingWizard)getWizard()).getSelectedPbiTracker().getId();
-				mappings = getMappings();
-				viewer.refresh();
+				refreshMappings();
+				setPageComplete(true);
 			}
 		}
-	}	
-	
-	private String[] getMappings() {
-		ProjectMappingWizard wizard = (ProjectMappingWizard)getWizard();
-		String trackerTaskMapping = wizard.getSelectedTaskTracker().getId() + "=>" + wizard.getSelectedProduct().getName() + "-Task";
-		String trackerPbiMapping = wizard.getSelectedPbiTracker().getId() + "=>" + wizard.getSelectedProduct().getName() + "-PBI";
-		String planningFolderProductMapping = wizard.getSelectedProject().getId() + "-planningFolders=>" + wizard.getSelectedProduct().getName() + "-Product";
-		String taskTrackerMapping = wizard.getSelectedProduct().getName() + "-Task=>" + wizard.getSelectedTaskTracker().getId();
-		String pbiTrackerMapping = wizard.getSelectedProduct().getName() + "-PBI=>" + wizard.getSelectedPbiTracker().getId();
-		String productPlanningFolderMapping = wizard.getSelectedProduct().getName() + "-Product=>" + wizard.getSelectedProject().getId() + "-planningFolders";
-		String[] mappings = { trackerTaskMapping, trackerPbiMapping, planningFolderProductMapping, taskTrackerMapping, pbiTrackerMapping, productPlanningFolderMapping };
-		return mappings;
 	}
 	
-	static class MappingsLabelProvider extends LabelProvider implements ITableLabelProvider {
+	public String getTrackerTaskMapping() {
+		return trackerTaskMapping;
+	}
 
-		public String getColumnText(Object element, int columnIndex) {
-			switch (columnIndex) { 
-				case 0: return element.toString();
-			}
-			return "";  //$NON-NLS-1$
-		}
-		
-		public Image getColumnImage(Object element, int columnIndex) {
-			return null;
-		}
+	public String getTrackerPbiMapping() {
+		return trackerPbiMapping;
+	}
+
+	public String getPlanningFolderProductMapping() {
+		return planningFolderProductMapping;
+	}
+
+	public String getTaskTrackerMapping() {
+		return taskTrackerMapping;
+	}
+
+	public String getPbiTrackerMapping() {
+		return pbiTrackerMapping;
+	}
+
+	public String getProductPlanningFolderMapping() {
+		return productPlanningFolderMapping;
+	}
+
+	public String getTrackerTaskConflictResolutionPriority() {
+		return SynchronizationStatus.CONFLICT_RESOLUTIONS[getSelectionIndex(trackerTaskCombo)];
+	}
 	
-	}	
+	public String getTrackerPbiConflictResolutionPriority() {
+		return SynchronizationStatus.CONFLICT_RESOLUTIONS[getSelectionIndex(trackerPbiCombo)];
+	}
 	
-	class MappingsContentProvider implements IStructuredContentProvider {
-		public void dispose() {
-		}
-		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-		}
-		public Object[] getElements(Object obj) {
-			if (mappings == null) {
-				return new String[0];
+	public String getPlanningFolderProductConflictResolutionPriority() {
+		return SynchronizationStatus.CONFLICT_RESOLUTIONS[getSelectionIndex(planningFolderProductCombo)];
+	}
+	
+	public String getTaskTrackerConflictResolutionPriority() {
+		return SynchronizationStatus.CONFLICT_RESOLUTIONS[getSelectionIndex(taskTrackerCombo)];
+	}
+	
+	public String getPbiTrackerConflictResolutionPriority() {
+		return SynchronizationStatus.CONFLICT_RESOLUTIONS[getSelectionIndex(pbiTrackerCombo)];
+	}
+	
+	public String getProductPlanningFolderConflictResolutionPriority() {
+		return SynchronizationStatus.CONFLICT_RESOLUTIONS[getSelectionIndex(productPlanningFolderCombo)];
+	}
+	
+	private int getSelectionIndex(final Combo combo) {
+		Display.getDefault().syncExec(new Runnable() {		
+			public void run() {
+				selectionIndex = combo.getSelectionIndex();
 			}
-			return mappings;
-		}
-	}	
+		});
+		return selectionIndex;
+	}
+	
+	private void populateConflictResolutionCombo(Combo combo) {
+		combo.add(SynchronizationStatus.CONFLICT_RESOLUTION_DESCRIPTION_ALWAYS_IGNORE);
+		combo.add(SynchronizationStatus.CONFLICT_RESOLUTION_DESCRIPTION_ALWAYS_OVERRIDE);
+		combo.add(SynchronizationStatus.CONFLICT_RESOLUTION_DESCRIPTION_QUARANTINE_ARTIFACT);
+		combo.setText(SynchronizationStatus.CONFLICT_RESOLUTION_DESCRIPTION_ALWAYS_OVERRIDE);
+	}
+	
+	private void refreshMappings() {
+		ProjectMappingWizard wizard = (ProjectMappingWizard)getWizard();
+		trackerTaskMapping = wizard.getSelectedTaskTracker().getId() + " => " + wizard.getSelectedProduct().getName() + "-Task";
+		trackerPbiMapping = wizard.getSelectedPbiTracker().getId() + " => " + wizard.getSelectedProduct().getName() + "-PBI";
+		planningFolderProductMapping = wizard.getSelectedProject().getId() + "-planningFolders => " + wizard.getSelectedProduct().getName() + "-Product";
+		taskTrackerMapping = wizard.getSelectedProduct().getName() + "-Task => " + wizard.getSelectedTaskTracker().getId();
+		pbiTrackerMapping = wizard.getSelectedProduct().getName() + "-PBI => " + wizard.getSelectedPbiTracker().getId();
+		productPlanningFolderMapping = wizard.getSelectedProduct().getName() + "-Product => " + wizard.getSelectedProject().getId() + "-planningFolders";
+		trackerTaskText.setText(trackerTaskMapping);
+		trackerPbiText.setText(trackerPbiMapping);
+		planningFolderProductText.setText(planningFolderProductMapping);
+		taskTrackerText.setText(taskTrackerMapping);
+		pbiTrackerText.setText(pbiTrackerMapping);
+		productPlanningFolderText.setText(productPlanningFolderMapping);
+	}
 
 }
